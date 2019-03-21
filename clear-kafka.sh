ff14259751f23fc69feb494acb978698a9138631
Daniel Cull <daniel@pearlai.com>
2019-03-21 14:56:24 +0000

    Updated clear kafka script to clear kafka then check for rebuild arg

--- clear-kafka.sh
+++ clear-kafka.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-[ -z "$1" ] && echo "First argument should be the the environment type to build" && exit 1
-
-if [[ $1 != "dev" ]] && [[ $1 != 'prod' ]]; then
-    echo "Please supply either dev or prod as the first argument" && exit 1
-fi
+echo "Deleteing the kafka namespaces, and therefore all objects within (pods, services etc)"
 
 kubectl delete ns kafka; kubectl delete ns test-kafka; kubectl delete pv --all;
 
 sleep 3
+
+[ -z "$1" ] && echo "First argument should be the the environment type to build" && exit 1
+
+if [[ $1 != "dev" ]] && [[ $1 != 'prod' ]]; then
+    echo "If you want to continue rebuilding the kafka environment please supply either dev or prod as the first argument" && exit 1
+fi
 
 ~/k8s-kafka-single-node/build-kafka.sh $1
